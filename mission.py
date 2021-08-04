bc4f02a7eb46503dd876505d994275c0a97edba8
Mrjiong-ff <56230028+Mrjiong-ff@users.noreply.github.com>
2021-08-04 15:57:38 +0800

    mission code

---
+++ mission.py
@@ -0,0 +1,15 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+url = 'https://www.roboxx.ltd/'
+targethtml = requests.get(url)
+soupdata = BeautifulSoup(targethtml.text,'html.parser')
+data=soupdata.select('#post-1727 > div > div.entry-after-image > header > h2 > a')
+for target in data:
+    result={
+        'title':target.get_text(),
+        'link':target.get('href'),
+        'ID':re.findall('\d+',target.get('href'))
+    }
+# print(data)
+print(result)
